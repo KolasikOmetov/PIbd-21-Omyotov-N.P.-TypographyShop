98415f51703472712104a7c060eb8733846db8bf
KolasikOmetov <42410594+KolasikOmetov@users.noreply.github.com>
2021-03-25 21:17:29 +0300

    implementation lab-2 superbase

--- TypographyShop/TypographyShopFileImplement/FileDataListSingleton.cs
+++ TypographyShop/TypographyShopFileImplement/FileDataListSingleton.cs
@@ -13,15 +13,18 @@ namespace TypographyShopFileImplement
 		private static FileDataListSingleton instance;
 		private readonly string ComponentFileName = "Component.xml";
 		private readonly string OrderFileName = "Order.xml";
+		private readonly string StoreFileName = "Store.xml";
 		private readonly string PrintedFileName = "Printed.xml";
 		public List<Component> Components { get; set; }
 		public List<Order> Orders { get; set; }
 		public List<Printed> Printeds { get; set; }
+		public List<Store> Stores { get; set; }
 		private FileDataListSingleton()
 		{
 			Components = LoadComponents();
 			Orders = LoadOrders();
 			Printeds = LoadPrinteds();
+			Stores = LoadStores();
 		}
 		public static FileDataListSingleton GetInstance()
 		{
@@ -36,6 +39,7 @@ namespace TypographyShopFileImplement
 			SaveComponents();
 			SaveOrders();
 			SavePrinteds();
+			SaveStores();
 		}
 		private List<Component> LoadComponents()
 		{
@@ -65,15 +69,15 @@ namespace TypographyShopFileImplement
 				foreach (var elem in xElements)
 				{
 					list.Add(new Order
-						{
-							Id = Convert.ToInt32(elem.Attribute("Id").Value),
-							PrintedId = Convert.ToInt32(elem.Element("PrintedId").Value),
-							Count = Convert.ToInt32(elem.Element("Count").Value),
-							Sum = Convert.ToDecimal(elem.Element("Sum").Value),
-							Status = (OrderStatus)Convert.ToInt32(elem.Element("Status").Value),
-							DateCreate = Convert.ToDateTime(elem.Element("DateCreate")?.Value),
-							DateImplement = String.IsNullOrEmpty(elem.Element("DateImplement").Value) ? DateTime.MinValue : Convert.ToDateTime(elem.Element("DateImplement").Value)
-						});
+					{
+						Id = Convert.ToInt32(elem.Attribute("Id").Value),
+						PrintedId = Convert.ToInt32(elem.Element("PrintedId").Value),
+						Count = Convert.ToInt32(elem.Element("Count").Value),
+						Sum = Convert.ToDecimal(elem.Element("Sum").Value),
+						Status = (OrderStatus)Convert.ToInt32(elem.Element("Status").Value),
+						DateCreate = Convert.ToDateTime(elem.Element("DateCreate")?.Value),
+						DateImplement = String.IsNullOrEmpty(elem.Element("DateImplement").Value) ? DateTime.MinValue : Convert.ToDateTime(elem.Element("DateImplement").Value)
+					});
 				}
 			}
 			return list;
@@ -98,6 +102,33 @@ namespace TypographyShopFileImplement
 						PrintedName = elem.Element("PrintedName").Value,
 						Price = Convert.ToDecimal(elem.Element("Price").Value),
 						PrintedComponents = prodComp
+					});
+				}
+			}
+			return list;
+		}
+		private List<Store> LoadStores()
+		{
+			var list = new List<Store>();
+			if (File.Exists(StoreFileName))
+			{
+				XDocument xDocument = XDocument.Load(StoreFileName);
+				var xElements = xDocument.Root.Elements("Store").ToList();
+				foreach (var elem in xElements)
+				{
+					var storeComponents = new Dictionary<int, (string, int)>();
+					foreach (var component in elem.Element("StoreComponents").Elements("StoreComponent").ToList())
+					{
+						var componentData = (component.Element("Component").Element("Name").Value, Convert.ToInt32(component.Element("Component").Element("Count").Value));
+						storeComponents.Add(Convert.ToInt32(component.Element("Key").Value), componentData);
+					}
+					list.Add(new Store
+					{
+						Id = Convert.ToInt32(elem.Attribute("Id").Value),
+						StoreName = elem.Element("StoreName").Value,
+						ResponsibleName = elem.Element("ResponsibleName").Value,
+						DateCreation = Convert.ToDateTime(elem.Element("DateCreation").Value),
+						StoreComponents = storeComponents
 					});
 				}
 			}
@@ -161,6 +192,35 @@ namespace TypographyShopFileImplement
 				}
 				XDocument xDocument = new XDocument(xElement);
 				xDocument.Save(PrintedFileName);
+			}
+		}
+		private void SaveStores()
+		{
+			if (Stores != null)
+			{
+				var xElement = new XElement("Stores");
+				foreach (var store in Stores)
+				{
+					var compElement = new XElement("StoreComponents");
+					foreach (var component in store.StoreComponents)
+					{
+						var element = new XElement("Component");
+						element.Add(
+							new XElement("Name", component.Value.Item1), new XElement("Count", component.Value.Item2)
+							);
+						compElement.Add(new XElement("StoreComponent",
+						new XElement("Key", component.Key),
+						element));
+					}
+					xElement.Add(new XElement("Store",
+					new XAttribute("Id", store.Id),
+					new XElement("StoreName", store.StoreName),
+					new XElement("ResponsibleName", store.ResponsibleName),
+					new XElement("DateCreation", store.DateCreation),
+					compElement));
+				}
+				XDocument xDocument = new XDocument(xElement);
+				xDocument.Save(StoreFileName);
 			}
 		}
 	}
