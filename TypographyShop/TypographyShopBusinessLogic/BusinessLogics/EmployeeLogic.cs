907b254c389fe4a12d32c67dbb9b945925d2d63a
KolasikOmetov <42410594+KolasikOmetov@users.noreply.github.com>
2021-04-15 09:19:59 +0300

    EmployeeLogic

---
+++ TypographyShop/TypographyShopBusinessLogic/BusinessLogics/EmployeeLogic.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypographyShopBusinessLogic.BusinessLogics
+{
+    public class EmployeeLogic
+    {
+        private readonly IEmployeeStorage _clientStorage;
+
+        public EmployeeLogic(IEmployeeStorage clientStorage)
+        {
+            _clientStorage = clientStorage;
+        }
+
+        public List<EmployeeViewModel> Read(EmployeeBindingModel model)
+        {
+            if (model == null)
+            {
+                return _clientStorage.GetFullList();
+            }
+            if (model.Id.HasValue)
+            {
+                return new List<EmployeeViewModel> { _clientStorage.GetElement(model) };
+            }
+            return _clientStorage.GetFilteredList(model);
+        }
+
+        public void CreateOrUpdate(EmployeeBindingModel model)
+        {
+            var element = _clientStorage.GetElement(new EmployeeBindingModel
+            {
+                Email = model.Email
+            });
+            if (element != null && element.Id != model.Id)
+            {
+                throw new Exception("Уже есть клиент с таким логином");
+            }
+            if (model.Id.HasValue)
+            {
+                _clientStorage.Update(model);
+            }
+            else
+            {
+                _clientStorage.Insert(model);
+            }
+        }
+
+        public void Delete(EmployeeBindingModel model)
+        {
+            var element = _clientStorage.GetElement(new EmployeeBindingModel
+            {
+                Id = model.Id
+            });
+            if (element == null)
+            {
+                throw new Exception("Клиент не найден");
+            }
+            _clientStorage.Delete(model);
+        }
+}
+}
