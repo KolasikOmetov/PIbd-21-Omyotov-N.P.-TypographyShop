ab2349205c9dc6fce11819737994f3237aa033e5
KolasikOmetov <42410594+KolasikOmetov@users.noreply.github.com>
2021-04-15 09:19:59 +0300

    EmployeeLogic

---
+++ TypographyShop/TypographyShopBusinessLogic/BusinessLogics/WorkModeling.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TypographyShopBusinessLogic.BindingModels;
+using TypographyShopBusinessLogic.Interfaces;
+using TypographyShopBusinessLogic.ViewModels;
+
+namespace TypographyShopBusinessLogic.BusinessLogics
+{
+    public class WorkModeling
+    {
+        private readonly IEmployeeStorage _employeeStorage;
+        private readonly IOrderStorage _orderStorage;
+        private readonly OrderLogic _orderLogic;
+        private readonly Random rnd;
+        public WorkModeling(IEmployeeStorage employeeStorage, IOrderStorage orderStorage, OrderLogic orderLogic)
+        {
+            this._employeeStorage = employeeStorage;
+            this._orderStorage = orderStorage;
+            this._orderLogic = orderLogic;
+            rnd = new Random(1000);
+        }
+        /// <summary>
+        /// Запуск работ
+        /// </summary>
+        public void DoWork()
+        {
+            var employees = _employeeStorage.GetFullList();
+            var orders = _orderStorage.GetFilteredList(new OrderBindingModel { FreeOrders = true });
+            foreach (var employee in employees)
+            {
+                WorkerWorkAsync(employee, orders);
+            }
+        }
+        /// <summary>
+        /// Иммитация работы исполнителя
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <param name="orders"></param>
+        private async void WorkerWorkAsync(EmployeeViewModel employee, List<OrderViewModel> orders)
+        {
+            // ищем заказы, которые уже в работе (вдруг исполнителя прервали)
+            var runOrders = await Task.Run(() => _orderStorage.GetFilteredList(new OrderBindingModel { EmployeeId = employee.Id }));
+            foreach (var order in runOrders)
+            {
+                // делаем работу заново
+                Thread.Sleep(employee.WorkingTime * rnd.Next(1, 5) * order.Count);
+                _orderLogic.FinishOrder(new ChangeStatusBindingModel { OrderId = order.Id });
+                // отдыхаем
+                Thread.Sleep(employee.PauseTime);
+            }
+            await Task.Run(() =>
+            {
+                foreach (var order in orders)
+                {
+                    // пытаемся назначить заказ на исполнителя
+                    try
+                    {
+                        _orderLogic.TakeOrderInWork(new ChangeStatusBindingModel { OrderId = order.Id, EmployeeId = employee.Id });
+                        // делаем работу
+                        Thread.Sleep(employee.WorkingTime * rnd.Next(1, 5) * order.Count);
+                        _orderLogic.FinishOrder(new ChangeStatusBindingModel { OrderId = order.Id });
+                        // отдыхаем
+                        Thread.Sleep(employee.PauseTime);
+                    }
+                    catch (Exception) { }
+                }
+            });
+        }
+
+    }
+}
